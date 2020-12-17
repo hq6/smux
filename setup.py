23fca2a3fadbc99568556f32598b574178d5321a
Henry Qin <root@hq6.me>
2020-12-17 01:52:42 -0800

    Add __init__.py to make package importable.

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ smux has a single API call ``create``::
 
 setup(
   name="smux.py",
-  version='0.1.1',
+  version='0.1.2',
   scripts=['smux.py'],
   author="Henry Qin",
   author_email="root@hq6.me",
